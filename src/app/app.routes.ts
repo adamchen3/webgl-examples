2c1ea6c42f7446c04d9a5237f6dd0e3afd2fe4f2
adamchen3 <weiguan.chen@outlook.com>
2023-11-30 22:18:57 +0800

    修改目录结构

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { TriangleComponent } from './triangle/triangle.component';
+import { TriangleComponent } from './webgl/triangle/triangle.component';
 import { MaterialPerformanceTestComponent } from './three/material-performance-test/material-performance-test.component';
 import { WorkerTestComponent } from './webgl/worker-test/worker-test.component';
 import { ColorTriangleComponent } from './webgl/color-triangle/color-triangle.component';
 
 export const routes: Routes = [
     {
-        path: 'triangle',
+        path: 'webgl-triangle',
         component: TriangleComponent,
 
     },
